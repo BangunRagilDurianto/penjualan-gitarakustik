4006dbe5da52407deb88d33fa163c9b743cce3ed
BangunRagilDurianto <210103050@mhs.udb.ac.id>
2022-11-16 09:58:32 +0700

    project crud

---
+++ tentang.php
@@ -0,0 +1,12 @@
+<br><center><h1 class="my-4 ">Tentang </h1></center>
+
+<div class="jumbotron">
+	<div style="margin-left:100px;">
+		<p>Toko gitar akustik online di Indonesia,dengan harga yang jauh<br> lebih murah dari toko lainnya.</p>
+		<p>Alamat : Jetak RT04/01 Jendi Selogiri Wonogiri</p>
+		<p>kode pos : 57652</p>
+		<p>WA : +62 819-0818-1173</p>
+		<p>Instagram : bangunrd_</p>
+		<p>Facebook : Bangun Ragil</p>
+	</div>
+</div>
